2d7c4f7ab7349d8a6839e198684d490a25061a30
Dominicod <dominicodonnell99@gmail.com>
2022-02-28 12:53:36 -0600

    Grabs JSON Data, and uploads into DB

---
+++ data.py
@@ -0,0 +1,24 @@
+import json
+import sqlite3
+from webbrowser import get
+
+# Connect to db
+try:
+    con = sqlite3.connect('', check_same_thread=False)
+    db = con.cursor()
+except:
+    print("Could not connect to Database")
+
+# Grab JSON data
+with open('', 'r') as read_file:
+    data = json.load(read_file)
+
+x = data["drinks"]
+count = 0
+
+# For every item inside of the JSON file, add to db
+for item in x:
+    item = x[count]['']
+    db.execute(f"ALTER TABLE user_bar ADD [{item}] BOOLEAN")
+    count += 1
+con.commit()
